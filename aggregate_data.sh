92a5ba04ed9b78a21f071d03c4a353c996f4170e
Sean Maguire <smmaguire@gmail.com>
2020-09-27 16:30:09 -0400

    update aggregate data
    switched the aggregation script to only make files that are approx 100,000 lines long (approximately 25,000 reads). Hopefully each individual part will run faster.

--- aggregate_data.sh
+++ aggregate_data.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
+#
+#$ -cwd
+#$ -j y
+#$ -S /bin/bash
+#$ -pe smp 1
+#$ -m e
 
-data_path=/mnt/nanopore/Sean_Maguire2/sRNA_nanopore_final/FC1_test_olio_mouse_brain/Final_seq_sRNA_naopore2/basecalled_fastq/
-agg_path=/home/smaguire/work/sRNA_circ/spades/data2/aggregated_data/
+data_path=/mnt/nanopore/Sean_Maguire2/sRNA_nanopore_final/FC2_mirX/basecalled/
+mkdir -p /home/smaguire/work/sRNA_circ/spades/aggregated_mirx_data/
+agg_path=/home/smaguire/work/sRNA_circ/spades/aggregated_mirx_data/
 
 for j in $( ls -d $data_path"barcode"*); do
     barc=${j#$data_path}
     mkdir -p $agg_path$barc
     file_count=0
-    count=0
     for i in $( ls $j/*.fastq); do
-        ((count++))
         cat $i >> $agg_path$barc/"agg_file_"$file_count".fastq"
-        if [ $count -gt 100 ]; then
-            count=0
+        line_count=`wc -l $agg_path$barc/"agg_file_"$file_count".fastq" | cut -f1 -d' '`
+        echo $line_count
+        if [ $line_count -gt 100000 ]; then
             ((file_count++))
         fi
     done
