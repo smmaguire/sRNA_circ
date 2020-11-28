c2bad44b1e27b5c0760c881035b989caad83e69b
Sean Maguire <smmaguire@gmail.com>
2020-11-28 12:53:29 -0500

    Update map_human_genome.sh

--- map_human_genome.sh
+++ map_human_genome.sh
@@ -34,7 +34,9 @@ echo "Count miRNAs"
 echo "#################################################"
 
 source activate htseq
-samtools sort $bc"_mapped.sam" > $bc"_mapped_sorted.sam"
-samtools index $bc"_mapped_sorted.sam"
+samtools view -b $bc"_mapped.sam" | samtools sort - > $bc"_mapped_sorted.bam"
+samtools index $bc"_mapped_sorted.bam"
+
 htseq-count --nonunique=all $bc"_mapped_sorted.sam" /home/smaguire/work/unblock_remakes/data/cancer_samples/genome/human_miRbase22.gtf > ${file_name}"_miRNA_counts.txt"
 htseq-count --nonunique=all $bc"_mapped_sorted.sam" /home/smaguire/work/sRNA_circ/spades/genome_ref/GRCh38_tRNA.gtf > ${file_name}"_tRNA_counts.txt"
+
